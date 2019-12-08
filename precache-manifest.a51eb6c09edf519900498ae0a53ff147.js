ca7604f5aebcb17b1f82dc46cfaa568b686638a3
YarikTheMashine <49390094+YarikTheMashine@users.noreply.github.com>
2019-12-08 22:53:41 +0200

    Updates

---
+++ precache-manifest.a51eb6c09edf519900498ae0a53ff147.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02d96c32c3acd4302fecca96935c215",
+    "url": "/hellopuffin/index.html"
+  },
+  {
+    "revision": "9a904d22210d0b84f6cf",
+    "url": "/hellopuffin/static/css/main.09f12208.chunk.css"
+  },
+  {
+    "revision": "fb600ac249d503285122",
+    "url": "/hellopuffin/static/js/2.dbb2287e.chunk.js"
+  },
+  {
+    "revision": "9a904d22210d0b84f6cf",
+    "url": "/hellopuffin/static/js/main.77c0cf84.chunk.js"
+  },
+  {
+    "revision": "4d094e8fb82b0fbfb83c",
+    "url": "/hellopuffin/static/js/runtime-main.4b18dbc6.js"
+  },
+  {
+    "revision": "91a75e9f1c7209d2da685c925916ee10",
+    "url": "/hellopuffin/static/media/b1c882bed79e.91a75e9f.png"
+  },
+  {
+    "revision": "9e5e57c478578e65ccf35d1d78a1e327",
+    "url": "/hellopuffin/static/media/error2.9e5e57c4.gif"
+  },
+  {
+    "revision": "5dcfbe09c5c3a716735bed78ba31ddb2",
+    "url": "/hellopuffin/static/media/logo1.5dcfbe09.png"
+  },
+  {
+    "revision": "4216de1e6b31968d567ed7e01042ab7e",
+    "url": "/hellopuffin/static/media/text.4216de1e.png"
+  }
+]);
